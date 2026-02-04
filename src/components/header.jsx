189fcefd55de25e32a5154ceb6663f28e7f17078
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-04 00:26:35 -0300

    realizo la estructura de la navbar

---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">My Tasks</h1>
+
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {/* <li>
+          <Link to="/tareas">Mis tareas</Link>
+        </li> */}
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/registrarme">
+            Registrarme
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export {Navbar};
