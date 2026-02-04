8cac8336d101093eb2e71b8a7cc866ff7d426ab3
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-04 00:24:20 -0300

    realizo la estructura basica de la pagina de registro

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,11 +1,68 @@
+import { Navbar } from "../components/header";
 import '../styles/views/home.css'
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth } from "../config/firebase.js";
 
 const Register = () => {
+  const [formDataUser, setFormDataUser] = useState({
+    email: "",
+    password: "",
+    user: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // evita que se recargue la página
+
+    // Acá ves lo que capturó el formulario
+    console.log("Email:", formDataUser);
+    createUserWithEmailAndPassword(auth, formDataUser.email, formDataUser.password)
+    
+  };
+
   return (
-    <div className="page Register">
-      <h1>Página de Registro</h1>
-      <p>Bienvenido a nuestra aplicación</p>
+    <>
+      <Navbar />
+      <div style={{ maxWidth: "400px", margin: "40px auto" }}>
+        <h2>Registrarse</h2>
+
+      <form onSubmit={handleSubmit}>
+         <div>
+          <label>Nombre de Usuario</label>
+          <input
+            type="text"
+            name="user"
+            value={formDataUser.user}
+            onChange={(e) => setFormDataUser({ ...formDataUser, user: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formDataUser.email}
+            onChange={(e) => setFormDataUser({ ...formDataUser, email: e.target.value })}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            value={formDataUser.password}
+            onChange={(e) => setFormDataUser({ ...formDataUser, password: e.target.value })}
+            required
+          />
+        </div>
+
+        <button type="submit">Registrarme</button>
+      </form>
     </div>
+  </>
   );
 }
 
