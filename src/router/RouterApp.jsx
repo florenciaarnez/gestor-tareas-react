4a0b2e48e1f5be1c8fbf7bcd7f24759fc4e861bc
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-06 18:20:34 -0300

    agrego proteccion a la pagina home

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "../views/Home"
 import { About } from "../views/AboutUs"
 import {Login} from "../views/Login"
+import {ErrorPage} from "../views/ErrorPage"
 import {Register} from "../views/Register"
+import { ProtectedRoute } from "../components/protectedRoute.jsx"
 
 const RouterApp = () => {
   return (
@@ -12,9 +14,8 @@ const RouterApp = () => {
         <Route path="/about" element={<About />} />
         <Route path="/registrarme" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-
-        {/* <Route path="/*" element={<NotFound />} /> */}
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/*" element={<ErrorPage/>} />
       </Routes>
     </BrowserRouter>
   )
