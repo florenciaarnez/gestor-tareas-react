2187749cf80b5c04280b1a2e2ee04f4c09f47295
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-06 03:42:26 -0300

    realizo las funcionalidades de login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,71 @@
 import '../styles/views/home.css'
+import { Navbar } from "../components/header";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth } from "../config/firebase.js";
 
 const Login = () => {
+    const [formDataUser, setFormDataUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // evita que se recargue la página
+    console.log("Email:", formDataUser);
+    try{
+      const userCredential = await signInWithEmailAndPassword(auth, formDataUser.email, formDataUser.password);
+      console.log("", userCredential);
+      setError(null); // Limpiar errores previos si la creación fue exitosa
+      setSuccess("Usuario logueado exitosamente");
+      // setUser(userCredential.user); // Guarda el usuario logueado en el estado
+      console.log("Usuario logueado:", userCredential.user);
+    }
+    catch(error){
+      console.error("Usuario o contraseña incorrectos:", error);
+      setSuccess(null);
+      setError("Error al loguearte");
+    }
+  };
+
   return (
-    <div className="page Login">
-      <h1>Página de Login</h1>
-      <p>Bienvenido a nuestra aplicación</p>
+    <>
+      <Navbar />
+      <div style={{ maxWidth: "400px", margin: "40px auto" }}>
+        <h2>Iniciar Sesion</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formDataUser.email}
+            onChange={(e) => setFormDataUser({ ...formDataUser, email: e.target.value })}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            value={formDataUser.password}
+            onChange={(e) => setFormDataUser({ ...formDataUser, password: e.target.value })}
+            required
+          />
+        </div>
+
+        <button type="submit">Ingresar</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {success && <p style={{ color: "green" }}>{success}</p>}
+      </form>
     </div>
+  </>
   );
 }
 
