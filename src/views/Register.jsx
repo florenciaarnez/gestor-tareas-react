44ae8f357bf9cd75f04861e88323efa9c813be12
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-06 02:38:31 -0300

    realizo las funcionalidades de Register con manejo de errores

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,8 +1,7 @@
+import '../styles/views/register.css'
 import { Navbar } from "../components/header";
-import '../styles/views/home.css'
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import {auth } from "../config/firebase.js";
+import { useAuth } from '../context/authContext.jsx';
 
 const Register = () => {
   const [formDataUser, setFormDataUser] = useState({
@@ -11,57 +10,87 @@ const Register = () => {
     user: ""
   });
 
-  const handleSubmit = (e) => {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const { register } = useAuth(); // => hook para usar el contexto de autenticación
+
+  const handleSubmit = async (e) => {
     e.preventDefault(); // evita que se recargue la página
 
-    // Acá ves lo que capturó el formulario
+    // data del usuario logueado
     console.log("Email:", formDataUser);
-    createUserWithEmailAndPassword(auth, formDataUser.email, formDataUser.password)
-    
-  };
+    try{
+
+      await register(formDataUser.email, formDataUser.password);
+    console.log("Usuario creado:");
+    setError(null); // Limpiar errores previos si la creación fue exitosa
+    setSuccess("Usuario creado exitosamente");
+
+    }
+    catch(error){
+
+      console.error("Error al crear el usuario:", error);
+
+      setSuccess(null);
+
+      let mensaje = "Error al crear el usuario";
+
+      //manejo de los distintos tipos de errores
+      
+      if (error.code === "auth/invalid-email") {
+      mensaje = "El email ingresado no es válido"
+
+    } else if (error.code === "auth/email-already-in-use") {
+      mensaje = "Este email ya está registrado"
+
+    } else if (error.code === "auth/weak-password") {
+      mensaje = "La contraseña debe tener al menos 6 caracteres"
+
+    } else if (error.code === "auth/missing-email") {
+      mensaje = "Tenés que ingresar un email"
+    }
+    setError(mensaje);
+  }
+};
 
   return (
     <>
       <Navbar />
-      <div style={{ maxWidth: "400px", margin: "40px auto" }}>
-        <h2>Registrarse</h2>
+      <div id="register">
+         <div className='register-container'>
+            <h2>Registrarse</h2>
 
-      <form onSubmit={handleSubmit}>
-         <div>
-          <label>Nombre de Usuario</label>
-          <input
-            type="text"
-            name="user"
-            value={formDataUser.user}
-            onChange={(e) => setFormDataUser({ ...formDataUser, user: e.target.value })}
-            required
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formDataUser.email}
-            onChange={(e) => setFormDataUser({ ...formDataUser, email: e.target.value })}
-            required
-          />
-        </div>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formDataUser.email}
+                  onChange={(e) => setFormDataUser({ ...formDataUser, email: e.target.value })}
+                  required
+                />
+              </div>
 
-        <div>
-          <label>Contraseña</label>
-          <input
-            type="password"
-            name="password"
-            value={formDataUser.password}
-            onChange={(e) => setFormDataUser({ ...formDataUser, password: e.target.value })}
-            required
-          />
-        </div>
+              <div>
+                <label>Contraseña</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formDataUser.password}
+                  onChange={(e) => setFormDataUser({ ...formDataUser, password: e.target.value })}
+                  required
+                />
+              </div>
 
-        <button type="submit">Registrarme</button>
-      </form>
-    </div>
+              <button type="submit">Registrarme</button>
+              {error && <p style={{ color: "red", marginTop: "10px", marginBottom: "10px" }}>{error}</p>}
+              {success && <p style={{ color: "green", marginTop: "10px", marginBottom: "10px" }}>{success}</p>}
+            </form>
+        </div>
+      </div>
+     
   </>
   );
 }
