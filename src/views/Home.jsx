f21df59b8521ee35821db33446d8273ebb0dfd3f
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-06 20:14:40 -0300

    realizo la estructura del home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,8 +5,76 @@ const Home = () => {
     <>
       <Navbar />
       <div className="page Home">
-        <h1>Página de Inicio</h1>
-        <p>Bienvenido a nuestra aplicación</p>
+          {/* FORM IZQUIERDO */}
+          <form className="taskForm">
+            <h3>Nueva tarea</h3>
+
+            <input type="text" placeholder="Título" />
+            <textarea placeholder="Descripción"></textarea>
+            <p>fecha limite: <input type="date" /></p>
+
+            <button type="submit">Agregar</button>
+          </form>
+        <div id="tasksContainer">
+          <div className="tasks">
+            <p>Zona de tareas</p>
+          </div>
+          <div className="tasks">
+            <h3>Titulo de la tarea</h3>
+            <p>ESTE seria el lugfar dond escribis tu tarea tengo que ver que entre todo perfecto</p>
+          </div>
+          <div className="tasks">
+            <h3>Titulo de la tarea</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+          </div>
+          <div className="tasks">
+            <h3>Titulo de la tarea</h3>
+            <p>Zona de tareas</p>
+          </div>
+          <div className="tasks">
+            <p>ESTE seria el lugfar dond escribis tu tarea tengo que ver que entre todo perfecto</p>
+          </div>
+          <div className="tasks">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+          </div>
+          <div className="tasks">
+            <p>Zona de tareas</p>
+          </div>
+          <div className="tasks">
+            <p>ESTE seria el lugfar dond escribis tu tarea tengo que ver que entre todo perfecto</p>
+          </div>
+          <div className="tasks">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+          </div>
+          <div className="tasks">
+            <p>Zona de tareas</p>
+          </div>
+          <div className="tasks">
+            <p>ESTE seria el lugfar dond escribis tu tarea tengo que ver que entre todo perfecto</p>
+          </div>
+          <div className="tasks">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+          </div>
+          <div className="tasks">
+            <p>Zona de tareas</p>
+          </div>
+          <div className="tasks">
+            <p>ESTE seria el lugfar dond escribis tu tarea tengo que ver que entre todo perfecto</p>
+          </div>
+          <div className="tasks">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+          </div>
+          <div className="tasks">
+            <p>Zona de tareas</p>
+          </div>
+          <div className="tasks">
+            <p>ESTE seria el lugfar dond escribis tu tarea tengo que ver que entre todo perfecto</p>
+          </div>
+          <div className="tasks">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+          </div>
+
+        </div>
       </div>
     </>
   );
