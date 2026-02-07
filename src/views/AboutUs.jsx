c34a0d3382c54edf00a8c9df4509d8adf2d82e26
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-06 22:40:03 -0300

    realizo estructura de la pagina about

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,12 +1,13 @@
-import '../styles/views/home.css'
+import '../styles/views/aboutUs.css'
 import { Navbar } from "../components/header";
 const About = () => {
   return (
     <>
       <Navbar />
-      <div className="page About">
-        <h1>Acerca de Nosotros</h1>
-        <p>Información sobre nuestra aplicación</p>
+      <div className="pageAbout">
+        <div id="presentation"><h1>Acerca de Nosotros</h1>
+        <p>My tasks es una aplicación para gestionar tareas de manera eficiente. Permitiendo al usuario Iniciar Sesion administrar sus tareas personales, ponerles fecha limite y mantener un registro de tareas completadas, poder visualizar su perfil y editarlo.</p>
+      </div>
       </div>
     </>
   );
