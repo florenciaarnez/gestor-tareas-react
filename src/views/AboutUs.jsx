0ac69b2ce4a1e982554a03207ffc7173ff78bf6e
Florencia Arnez <florenciaarnez.ar@gmail.com>
2026-02-20 09:28:49 -0300

    inicio el estilo de la pagina estatia about us

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -6,8 +6,17 @@ const About = () => {
       <Navbar />
       <div className="pageAbout">
         <div id="presentation"><h1>Acerca de Nosotros</h1>
-        <p>My tasks es una aplicación para gestionar tareas de manera eficiente. Permitiendo al usuario Iniciar Sesion administrar sus tareas personales, ponerles fecha limite y mantener un registro de tareas completadas, poder visualizar su perfil y editarlo.</p>
+        <p>My tasks es una aplicación para gestionar tareas de manera eficiente. Permitiendo al usuario loguearse y visualizar únicamente sus tareas, con la posibilidad de filtrar por tareas completadas, retrasadas o en proceso, siendo una herramienta para visualizar el progreso de cada uno. En cada tarea el usuario puede visualizar cuando fue creada la descripcion de esta y la fecha para la cual esta deberia estar terminada</p>
+        <h2>Tecnologías Utilizadas:</h2>
+        <ul>
+          <li>React: Para la interfaz de usuario con JSX </li>
+          <li>React Router: Para la navegación entre diferentes paginas</li>
+
+          <li>Firebase: Para la autenticación (Firebase Authentication) y almacenamiento de datos. Tambien utilizandola como servicio de backend</li>
+          <li>CSS: Para el diseño y estilo de la aplicación, Responsive utilizando flex box y grid</li>
+        </ul>
       </div>
+      <img src="../../public/estructura.png" alt="Fondo de papel" />
       </div>
     </>
   );
